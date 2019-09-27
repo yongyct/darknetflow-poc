be3f96ffb27e7a63e2439fc9c2989ae7388b187e
Tommy Yong <yongyct@gmail.com>
2019-09-27 09:12:16 +0800

    added some base packages for models

---
+++ darknetflow_poc/models/networks/basenet.py
@@ -0,0 +1,6 @@
+class BaseNet:
+    """
+    Base class for forming other network topologies
+    """
+    def __init__(self):
+        print('Calling BaseNet')
---
+++ darknetflow_poc/models/networks/tomnet.py
@@ -0,0 +1,9 @@
+from darknetflow_poc.models.networks.basenet import BaseNet
+
+
+class TomNet(BaseNet):
+    """
+    Sample network
+    """
+    def __init__(self):
+        super(TomNet, self).__init__()
